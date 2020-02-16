e0a1b786204cfbf24cf86258de8a6858ebe5d90f
Karolina Frostare <karolina.frostare@gmail.com>
2020-02-16 09:20:54 +0100

    trying out the seeds.rb to add articles to the database because I'm too lazy to make a create-button

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,16 +9,6 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
-    respond_to do |format|
-      if @article.save
-        format.html { redirect_to @show, notice: 'Article was successfully created!' }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def delete
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.create!( title: 'An awesome first title', content: 'The best first content ever')
