5cdfff54315291cf443d63a9ee48e9c7e9087d5b
Karolina Frostare <karolina.frostare@gmail.com>
2020-02-16 11:38:44 +0100

    update article_params content argument

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   end
 
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :content)
   end
 
   def update #save
